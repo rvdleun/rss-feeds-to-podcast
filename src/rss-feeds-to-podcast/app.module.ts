37bba524a837dcf0b68a4b477a4f2f87e2cecce4
Ruben van der Leun <ruben@leunix.nl>
2025-07-10 22:16:30 +0200

    chore: renamed commands to workflow

--- src/rss-feeds-to-podcast/app.module.ts
+++ src/rss-feeds-to-podcast/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './modules/config/config.module';
-import { RssFeedModule } from './commands/rss-feed/rss-feed.module';
-import { SegmentPickerModule } from './commands/segment-picker/segment-picker.module';
-import { ContentScraperModule } from './commands/content-scraper/content-scraper.module';
+import { RssFeedModule } from './workflow/1-rss-feed/rss-feed.module';
+import { SegmentPickerModule } from './workflow/2-segment-picker/segment-picker.module';
+import { ContentScraperModule } from './workflow/3-content-scraper/content-scraper.module';
 
 @Module({
   imports: [RssFeedModule, SegmentPickerModule, ContentScraperModule],
